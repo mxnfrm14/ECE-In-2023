1dad51c4a1d6bebfbbc0f0633e0fbca7829ccceb
mxnfrm14 <96044322+mxnfrm14@users.noreply.github.com>
2023-06-04 17:27:09 +0200

    visio a ajouté

--- components/EcrireMsg.js
+++ components/EcrireMsg.js
@@ -1,8 +1,11 @@
+import {BsCameraVideo} from 'react-icons/bs';
+
 function EcrireMsg({ vari }) {
     
     var zone=
-    <div className="m-4 mt-4 max-w-md mx-auto">
-       <input type="text" placeholder="Type here" className="input input-bordered input-primary w-3/4 max-w-lg m-1" />
+    <div className="m-4 mt-4 max-w-lg mx-auto">
+        <button className="btn btn-primary m-1"><BsCameraVideo/></button>
+       <input type="text" placeholder="Type here" className="input input-bordered input-primary w-2/4 max-w-lg m-1" />
        <button className="btn btn-outline btn-primary m-1">Envoyer</button>
     </div>
     ; 
