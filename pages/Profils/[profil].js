c1e63ed0616a42d21e310f28a628f595083c8a34
mxnfrm14 <96044322+mxnfrm14@users.noreply.github.com>
2023-06-04 23:39:32 +0200

    PROFIL TABLEAU RESEAU

--- pages/Profils/[profil].js
+++ pages/Profils/[profil].js
@@ -11,16 +11,18 @@ export default function Profil(props) {
   const [loading, setLoading] = useState(true);
   const [utilisateur, setUtilisateur] = useState([]);
 
+
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const author = data.USERID;
+        const author = JSON.parse(localStorage.getItem('user')).id;
         const response = await fetch(`/api/getUser?IDENTIFIANT=${author}`);
         const data = await response.json();
-        const user_data = data.results || [];
-        setUtilisateur(user_data);
+        const user_data = data.results;   
+        console.log(user_data);
         setLoading(false);
-        setUtilisateur(user_data[0]);
+        setUtilisateur(user_data);
 
         console.log('-----------------------------------------------');
         console.log(data.USERID);
@@ -35,22 +37,22 @@ export default function Profil(props) {
   return (
     <>
       <Head>
-        <title>Profil {utilisateur.PSEUDO} | ECE In</title>
+        <title>Profil {utilisateur[0]?.PSEUDO} | ECE In</title>
       </Head>
 
       <Layout>
         <div className="m-4">
           <div className="grid grid-cols-5 gap-x-2 justify-items-center">
             <div className="col-span-2 row-span-2">
-              <h1 className="m-5 text-5xl font-bold mt-6">Profil de {utilisateur.PSEUDO}</h1>
+              <h1 className="m-5 text-5xl font-bold mt-6">Profil de {utilisateur[0].PSEUDO}</h1>
 
               <UserPresentationCard
-                identifiant={utilisateur.IDENTIFIANT}
-                nom={utilisateur.NOM}
-                pseudo={utilisateur.PSEUDO}
-                descript={utilisateur.DESCRIPTION}
-                pfp={utilisateur.PHOTO}
-                imgfond={utilisateur.BACKIMG}
+                identifiant={utilisateur[0].IDENTIFIANT}
+                nom={utilisateur[0].NOM}
+                pseudo={utilisateur[0].PSEUDO}
+                descript={utilisateur[0].DESCRIPTION}
+                pfp={utilisateur[0].PHOTO ? `/` + utilisateur[0].PHOTO : ''}
+                imgfond={utilisateur[0].BACKIMG ? `/` + utilisateur[0].BACKIMG : ''}
               />
             </div>
           </div>
@@ -60,29 +62,32 @@ export default function Profil(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const response = await fetch(`http://localhost:3000/api/getUser?IDENTIFIANT=${context.params.user}`);
-  const data = await response.json();
-  const user_data = data.results[0];
+// export async function getStaticProps(context) {
+//   const response = await fetch(`http://localhost:3000/api/getUser?IDENTIFIANT=${context.params.user}`);
+//   const data = await response.json();
+//   const user_data = data.results[0];
 
-  return {
-    props: {
-      user_data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       user_data,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const response = await fetch(`http://localhost:3000/api/getUsers`);
-  const data = await response.json();
-  const users = data.results;
+// export async function getStaticPaths() {
+//   const router = useRouter();
+//   const { userQ } = router.query;
 
-  const paths = users.map((user) => ({
-    params: { user: user.USERID.toString() },
-  }));
+//   const response = await fetch(`http://localhost:3000/api/getUser?IDENTIFIANT=${userQ}`);
+//   const data = await response.json();
+//   const users = data.results;
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   const paths = users.map((user) => ({
+//     params: { user: user.USERID.toString() },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
